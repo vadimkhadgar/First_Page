9f339aa087f4603a8fbb015789200a5898ff220f
Vadim Bliashuk <vadim.bliashuk@orangesoft.co>
2020-06-18 14:15:09 +0300

    added proile item to bottom navigation view

--- app/src/main/java/com/mobile/instagramfirstpage/MainActivity.kt
+++ app/src/main/java/com/mobile/instagramfirstpage/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.mobile.instagramfirstpage
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.mobile.instagramfirstpage.adapters.NewsAdapter
 import com.mobile.instagramfirstpage.adapters.StoriesAdapter
 import com.mobile.instagramfirstpage.data.NewsDataSource
 import com.mobile.instagramfirstpage.data.StoryDataSource
+import com.mobile.instagramfirstpage.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view1 = binding.root
+        setContentView(view1)
         setSupportActionBar(toolbar)
 
         val layoutManager = LinearLayoutManager(this)
@@ -23,5 +34,13 @@ class MainActivity : AppCompatActivity() {
         val adapter2 = NewsAdapter(NewsDataSource.news())
         recyclerViewNews.layoutManager = layoutManager
         recyclerViewNews.adapter = adapter2
+
+
+        val bottomNavigationMenuView: BottomNavigationMenuView = binding.bottomNavigation.getChildAt(0) as BottomNavigationMenuView
+        val view = bottomNavigationMenuView.getChildAt(4)
+        val bottomNavigationItemView = view as BottomNavigationItemView
+        val profileItem: View = LayoutInflater.from(view1.context).inflate(R.layout.menu_profile_layout, bottomNavigationItemView, false)
+        bottomNavigationItemView.addView(profileItem)
+
     }
 }
