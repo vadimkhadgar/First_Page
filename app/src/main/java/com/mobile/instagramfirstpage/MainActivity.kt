a147fd8b41b60a58387708ea1e553fb939349e06
Vadim Bliashuk <vadim.bliashuk@orangesoft.co>
2020-06-19 12:10:30 +0300

    added functionality to bottom navigation view

--- app/src/main/java/com/mobile/instagramfirstpage/MainActivity.kt
+++ app/src/main/java/com/mobile/instagramfirstpage/MainActivity.kt
@@ -1,55 +1,51 @@
 package com.mobile.instagramfirstpage
 
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Adapter
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.bumptech.glide.Glide
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView
-import com.mobile.instagramfirstpage.adapters.DataItem
-import com.mobile.instagramfirstpage.adapters.NewsAdapter
-import com.mobile.instagramfirstpage.adapters.StoriesAdapter
-import com.mobile.instagramfirstpage.data.NewsDataSource
-import com.mobile.instagramfirstpage.data.StoryDataSource
 import com.mobile.instagramfirstpage.databinding.ActivityMainBinding
-import com.mobile.instagramfirstpage.model.Story
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+//    private lateinit var appBarConfig: AppBarConfiguration
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view1 = binding.root
         setContentView(view1)
-        setSupportActionBar(toolbar)
 
-        val layoutManager = LinearLayoutManager(this)
-        val adapter =
-            StoriesAdapter()
-        recyclerViewStories.adapter = adapter
-        StoryDataSource.stories().let {
-            adapter.addHeaderAndSubmitList(it)
-        }
-
-        val adapter2 = NewsAdapter(NewsDataSource.news())
-        recyclerViewNews.layoutManager = layoutManager
-        recyclerViewNews.adapter = adapter2
+        navController = Navigation.findNavController(
+            this,
+            R.id.mainContainer
+        )
+        binding.bottomNavigation.setupWithNavController(navController)
 
         // For image in bottom navigation view
-        val bottomNavigationMenuView: BottomNavigationMenuView = binding.bottomNavigation.getChildAt(0) as BottomNavigationMenuView
+        val bottomNavigationMenuView: BottomNavigationMenuView =
+            binding.bottomNavigation.getChildAt(0) as BottomNavigationMenuView
         val view = bottomNavigationMenuView.getChildAt(4)
         val bottomNavigationItemView = view as BottomNavigationItemView
-        val profileItem: View = LayoutInflater.from(view1.context).inflate(R.layout.menu_profile_layout, bottomNavigationItemView, false)
+        val profileItem: View = LayoutInflater.from(view1.context)
+            .inflate(R.layout.menu_profile_layout, bottomNavigationItemView, false)
         bottomNavigationItemView.addView(profileItem)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.mainContainer).navigateUp() || super.onSupportNavigateUp()
 
     }
 
---
+++ app/src/main/java/com/mobile/instagramfirstpage/ui/home/HomeFragment.kt
@@ -0,0 +1,53 @@
+package com.mobile.instagramfirstpage.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.mobile.instagramfirstpage.MainActivity
+import com.mobile.instagramfirstpage.R
+import com.mobile.instagramfirstpage.adapters.NewsAdapter
+import com.mobile.instagramfirstpage.adapters.StoriesAdapter
+import com.mobile.instagramfirstpage.data.NewsDataSource
+import com.mobile.instagramfirstpage.data.StoryDataSource
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        (activity as MainActivity).setSupportActionBar(toolbar)
+
+        val layoutManager = LinearLayoutManager(requireContext())
+        val adapter =
+            StoriesAdapter()
+        recyclerViewStories.adapter = adapter
+        StoryDataSource.stories()?.let {
+            adapter.addHeaderAndSubmitList(it)
+        }
+
+        val adapter2 = NewsAdapter(NewsDataSource.news())
+        recyclerViewNews.layoutManager = layoutManager
+        recyclerViewNews.adapter = adapter2
+    }
+}
