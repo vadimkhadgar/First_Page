8889032147456974431c1498fcbf60823e854a59
Vadim Bliashuk <vadim.bliashuk@orangesoft.co>
2020-06-19 14:08:58 +0300

    refactoring

--- app/src/main/java/com/mobile/instagramfirstpage/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mobile/instagramfirstpage/ui/home/HomeFragment.kt
@@ -4,19 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.bottomnavigation.BottomNavigationItemView
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.mobile.instagramfirstpage.MainActivity
 import com.mobile.instagramfirstpage.R
 import com.mobile.instagramfirstpage.adapters.NewsAdapter
 import com.mobile.instagramfirstpage.adapters.StoriesAdapter
 import com.mobile.instagramfirstpage.data.NewsDataSource
-import com.mobile.instagramfirstpage.data.StoryDataSource
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
@@ -29,7 +25,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         return root
     }
