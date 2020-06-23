50de734b422ac0a426b1ebd22e0f2d9eabb8190f
Vadim Bliashuk <vadim.bliashuk@orangesoft.co>
2020-06-23 11:23:39 +0300

    added splash theme

--- app/src/main/java/com/mobile/instagramfirstpage/MainActivity.kt
+++ app/src/main/java/com/mobile/instagramfirstpage/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view1 = binding.root
         setContentView(view1)
